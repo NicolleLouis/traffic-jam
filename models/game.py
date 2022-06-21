e2f934f4d5a3464c346d71266e46f17a6bf58696
NicolleLouis <louisxnicolle@gmail.com>
2022-06-21 22:21:00 +0200

    Nice visual

--- models/game.py
+++ models/game.py
@@ -7,27 +7,43 @@ class Game:
         self.grid = Grid(size=10)
         self.roads = [
             Road(grid=self.grid, x=4),
+            Road(grid=self.grid, y=4),
         ]
         Car(
             grid=self.grid,
             road=self.roads[0],
             position=self.roads[0].positions[0]
         )
+        Car(
+            grid=self.grid,
+            road=self.roads[0],
+            position=self.roads[0].positions[1]
+        )
+        Car(
+            grid=self.grid,
+            road=self.roads[1],
+            position=self.roads[1].positions[0]
+        )
+        Car(
+            grid=self.grid,
+            road=self.roads[1],
+            position=self.roads[1].positions[1]
+        )
 
     def step(self):
         for grid_object in self.grid.objects:
             grid_object.step()
 
-    def run(self, number_of_turns=20):
+    def run(self, number_of_turns=10):
         for turn_number in range(number_of_turns):
             print(f"Turn Number: {turn_number}")
-            self.display_information(display_road=True)
+            self.display_information()
             self.step()
-            print("#####")
 
     def display_information(self, display_road=False):
         if display_road:
             self.display_road_data()
+        self.display_grid()
 
     def display_road_data(self):
         print("Roads Data:")
@@ -36,3 +52,6 @@ class Game:
             print("#####")
             print(f"Road Number {index}:")
             road.display_data()
+
+    def display_grid(self):
+        self.grid.display_grid()
--- models/grid.py
+++ models/grid.py
@@ -1,7 +1,10 @@
+from models import Position
+
+
 class Grid:
     def __init__(self, size=50):
         self.width = size
-        self.length = size
+        self.height = size
         self.objects = []
 
     def add_object(self, grid_object):
@@ -12,3 +15,17 @@ class Grid:
             if grid_object.position == position:
                 return False
         return True
+
+    def display_grid(self):
+        for y in range(self.height):
+            self.display_line(y=y)
+
+    def display_line(self, y):
+        line_representation = ""
+        for x in range(self.width):
+            position = Position(x=x, y=y)
+            if self.is_position_empty(position):
+                line_representation += " "
+            else:
+                line_representation += "#"
+        print(line_representation)
